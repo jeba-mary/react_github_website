a05ab0539768c19275113bd579ef67aa1d9caf1b
Maryjeba <maryjeba11@gmail.com>
2020-09-23 12:45:06 +0530

    Updates

---
+++ precache-manifest.6e4e1a64071fc589d39a4b4f90f17eb8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a1ef61e5d0e9df2eae8bdf5bec5166d",
+    "url": "/react_github_website/index.html"
+  },
+  {
+    "revision": "7fd6e4c7d747fafb08a7",
+    "url": "/react_github_website/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6b65f66a1a504253ae46",
+    "url": "/react_github_website/static/js/2.562a04d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_github_website/static/js/2.562a04d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fd6e4c7d747fafb08a7",
+    "url": "/react_github_website/static/js/main.3c95a637.chunk.js"
+  },
+  {
+    "revision": "29fbedb5fa7f991d74a4",
+    "url": "/react_github_website/static/js/runtime-main.b8364315.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_github_website/static/media/logo.5d5d9eef.svg"
+  }
+]);
